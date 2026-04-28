097e7701ecea432706dd6b0e3b25ff42772fed27
Kireeti <kireeti@ggwp.com>
2026-04-28 21:45:32 +0530

    Completed hamming and downloaded additional exercs

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,19 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("Strands must be of equal length")
+	}
+
+	distance := 0
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance += 1
+		}
+	}
+
+	return distance, nil
 }
